d6e41aa366f500b4c5a5dd9dc17348ef49e576b6
shakiller <shakiller@tut.by>
2019-10-22 22:08:49 +0300

    beautify

--- public/_site/index.js
+++ public/_site/index.js
@@ -8,73 +8,101 @@ var bodyBG = "img/bg_map.png";
 var HUEdeg = 0;
 var BGposX = 0;
 var checkStartCell;
-var correctWays=false;
-var audioBG=new Audio("aud/ambient for maze game.mp3");
-var audioClick=new Audio("aud/Ding-ding-sound.mp3");
+var correctWays = false;
+var audioBG = new Audio("aud/ambient for maze game.mp3");
+var audioClick = new Audio("aud/Ding-ding-sound.mp3");
 
 playSoundStart();
 
-function playSoundBG(){
+function playSoundBG() {
     audioBG.play();
     var sndVoff = "<span class='snd'>&#9834;</span> <input type='button' class='snd' value='-' id='snfOfOff' onclick='vSoundOff()'>";
     var sndVon = "<input type='button' class='snd' value='+' id='snfOfOn' onclick='vSoundOn()'>";
-    document.getElementById('UI').innerHTML+=sndVoff+sndVon;
+    document.getElementById('UI').innerHTML += sndVoff + sndVon;
 }
 
-function playSoundClick(){
-    switch(Math.floor(Math.random()*12)+1){
-    case 1:audioClick=new Audio("aud/n1.mp3");break;
-    case 2:audioClick=new Audio("aud/n2.mp3");break;
-    case 3:audioClick=new Audio("aud/n3.mp3");break;
-    case 4:audioClick=new Audio("aud/n4.mp3");break;
-    case 5:audioClick=new Audio("aud/n5.mp3");break;
-    case 6:audioClick=new Audio("aud/n6.mp3");break;
-    case 7:audioClick=new Audio("aud/n7.mp3");break;
-    case 8:audioClick=new Audio("aud/n8.mp3");break;
-    case 9:audioClick=new Audio("aud/n9.mp3");break;
-    case 10:audioClick=new Audio("aud/n10.mp3");break;
-    case 11:audioClick=new Audio("aud/n11.mp3");break;
-    case 12:audioClick=new Audio("aud/n12.mp3");break;
-    
+function playSoundClick() {
+    switch (Math.floor(Math.random() * 12) + 1) {
+        case 1:
+            audioClick = new Audio("aud/n1.mp3");
+            break;
+        case 2:
+            audioClick = new Audio("aud/n2.mp3");
+            break;
+        case 3:
+            audioClick = new Audio("aud/n3.mp3");
+            break;
+        case 4:
+            audioClick = new Audio("aud/n4.mp3");
+            break;
+        case 5:
+            audioClick = new Audio("aud/n5.mp3");
+            break;
+        case 6:
+            audioClick = new Audio("aud/n6.mp3");
+            break;
+        case 7:
+            audioClick = new Audio("aud/n7.mp3");
+            break;
+        case 8:
+            audioClick = new Audio("aud/n8.mp3");
+            break;
+        case 9:
+            audioClick = new Audio("aud/n9.mp3");
+            break;
+        case 10:
+            audioClick = new Audio("aud/n10.mp3");
+            break;
+        case 11:
+            audioClick = new Audio("aud/n11.mp3");
+            break;
+        case 12:
+            audioClick = new Audio("aud/n12.mp3");
+            break;
+
     }
     audioClick.play();
 }
 
-function playSoundStart(){
-    audioClick=new Audio("aud/Ding-ding-sound.mp3");
+function playSoundStart() {
+    audioClick = new Audio("aud/Ding-ding-sound.mp3");
     audioClick.play();
 }
 
-function vSoundOff(){
-    function sndDn(){
-        var myintervalSD = setInterval(function(){
-           if(audioBG.volume >= 0.01){audioBG.volume -= 0.01;}
-           if(audioBG.volume <= 0.01)clearInterval(myintervalSD);
-       },30);
-   
-       console.log(audioBG.volume);
-   }
-   
-   sndDn();
+function vSoundOff() {
+    function sndDn() {
+        var myintervalSD = setInterval(function () {
+            if (audioBG.volume >= 0.01) {
+                audioBG.volume -= 0.01;
+            }
+            if (audioBG.volume <= 0.01) clearInterval(myintervalSD);
+        }, 30);
 
-    document.getElementById('snfOfOff').setAttribute('style','opacity:0.1;');
-    document.getElementById('snfOfOn').setAttribute('style','opacity:1;');
+        console.log(audioBG.volume);
+    }
+
+    sndDn();
+
+    document.getElementById('snfOfOff').setAttribute('style', 'opacity:0.1;');
+    document.getElementById('snfOfOn').setAttribute('style', 'opacity:1;');
 }
 
-function vSoundOn(){
-    function sndUp(){
-     var myintervalSU = setInterval(function(){
-        if(audioBG.volume <= 0.8){audioBG.volume += 0.01;}
-        if(audioBG.volume >= 0.8)clearInterval(myintervalSU);
-    },30);
+function vSoundOn() {
+    function sndUp() {
+        var myintervalSU = setInterval(function () {
+            if (audioBG.volume <= 0.8) {
+                audioBG.volume += 0.01;
+            }
+            if (audioBG.volume >= 0.8) clearInterval(myintervalSU);
+        }, 30);
 
-    console.log(audioBG.volume);
-}
+        console.log(audioBG.volume);
+    }
 
-sndUp();
-    
-    document.getElementById('snfOfOff').setAttribute('style','opacity:1;');
-    document.getElementById('snfOfOn').setAttribute('style','opacity:0.1;');
+    sndUp();
+
+    document.getElementById('snfOfOff').setAttribute('style', 'opacity:1;');
+    document.getElementById('snfOfOn').setAttribute('style', 'opacity:0.1;');
 }
 
 function createTable(sizeX, sizeY) {
@@ -90,46 +118,42 @@ function createTable(sizeX, sizeY) {
     table.setAttribute('id', 'playField');
     document.getElementById('Field').appendChild(table);
     pF = document.getElementById('playField');
-    
+
     start_end_rand();
-    
+
 }
 
 
 
 function setSize(n) {
-    size=n;
+    size = n;
     setTimeout(popupAnimate, 1);
     createTable(size, size);
-    
+
     startGame();
     setTimeout(animateStart, 1);
-    
+
 }
-function popupAnimate(){
-    document.getElementById('popup').setAttribute('style','margin-top:-1800px;');
+
+function popupAnimate() {
+    document.getElementById('popup').setAttribute('style', 'margin-top:-1800px;');
     playSoundBG();
 }
 
-function animateStart(){
-    document.getElementById('playField').setAttribute("class","animateStart")
+function animateStart() {
+    document.getElementById('playField').setAttribute("class", "animateStart")
 }
 
 
-
-function createPopup_Select_level(){
+function createPopup_Select_level() {
     var popup = document.createElement('div');
-    popup.setAttribute('id','popup');
-    popup.innerHTML='<h2>выберите уровень сложности</h2>';
-    document.getElementById('Field').appendChild(popup); 
-    popup.innerHTML=popup.innerHTML+'<input type="button" value="1" id="v1" onclick = "setSize(5)"> <input type="button" value="2"  id="v2" onclick = "setSize(7)"> <input type="button" value="3"  id="v3" onclick = "setSize(10)">';
-    
-    
-    
-    
+    popup.setAttribute('id', 'popup');
+    popup.innerHTML = '<h2>выберите уровень сложности</h2>';
+    document.getElementById('Field').appendChild(popup);
+    popup.innerHTML = popup.innerHTML + '<input type="button" value="1" id="v1" onclick = "setSize(5)"> <input type="button" value="2"  id="v2" onclick = "setSize(7)"> <input type="button" value="3"  id="v3" onclick = "setSize(10)">';
+
 }
 setTimeout(createPopup_Select_level, 500);
-
 
 
 
@@ -152,7 +176,7 @@ function Board() {
         for (cell in this.table) {
 
 
-            var top = this["table"][+cell].top; 
+            var top = this["table"][+cell].top;
             var topParent = () => {
                 if (+cell <= (size - 1)) {
                     return 0
@@ -194,7 +218,7 @@ function Board() {
             if (!checkTop || !checkRight || !checkBottom || !checkLeft) {
                 console.log("fail");
                 document.getElementById("win").innerHTML = " ";
-                correctWays=false;
+                correctWays = false;
                 break;
             } else var a = "prostotak";
         }
@@ -291,14 +315,16 @@ function freeways(innerId, outerId) {
 
 function start_end_rand() {
     current_figure_rand();
-    start = Math.floor(Math.random() * size);console.log(start," start");
-    end = Math.floor(Math.random() * size);console.log(end," end");
+    start = Math.floor(Math.random() * size);
+    console.log(start, " start");
+    end = Math.floor(Math.random() * size);
+    console.log(end, " end");
     var start_block = pF.rows[start].cells[0];
     var end_block = pF.rows[end].cells[(size - 1)];
     freeways(start, end);
     startId = start * size;
     endId = end * size + size - 1;
-    console.log(startId," ",endId," s e");
+    console.log(startId, " ", endId, " s e");
     board.writeChanges(startId, true, start_ways);
     board.writeChanges(endId, true, end_ways);
     start_block.setAttribute("id", "start");
@@ -311,105 +337,107 @@ function start_end_rand() {
 
 document.getElementById("body").setAttribute('style', "background-image:url(" + bodyBG + ");background-size:cover;background-position:" + BGposX + "% 50%;");
 var id = 0
-function startGame(){
-pF.querySelectorAll('td').forEach(function (e) {
-    console.log(e);
-    
-    e.setAttribute('data-id', id++);
-    e.onclick = function () {
-        document.getElementById("playField").setAttribute('style', "background-image:url(" + bg + ");background-size:cover;background-position:center;");
 
-        HUEdeg = HUEdeg + 5;
-        playSoundClick();
-        var setAttr = "tdblock bl " + current_figure_id;
-        var tempThis = this;
-        this.setAttribute('class', setAttr);
-        var id = this.dataset.id;
-        if ((id == endId) || (id == startId)) {
-            return
-        }
-        board.writeChanges(id, false);
-        document.getElementById("move").innerHTML = ++move;
-        document.getElementById(current_figure_id).setAttribute('style', 'filter:sepia(100%);order:0; z-index:0;transform:translateY(0px);background-image: url(img/card_general.png);');
-        document.getElementById("body").setAttribute('style', "background-image:url(" + bodyBG + ");background-size:cover;background-position:" + BGposX + "% 50%;");
-        if (BGposX < 100) {
-            BGposX++
-        } else {
-            BGposX = 0
-        };
-        document.getElementById("win").innerHTML = "win";
-        correctWays=true;
-        for (var i = 0; i <= (size * size - 1); i++) {
-            pF.rows[Math.floor((i) / size)].cells[(i) % size].setAttribute('style', 'background:' + FULLtransparent + ';');
-        }
-        current_figure_rand();
-        board.checkWin();
-        var count = 0;
-        if (board["table"][endId] == "start") {
-            document.getElementById("win").innerHTML = "!!!";
-            return;
-        }
-        for (cell in board["table"]) {
-            board["table"][cell].start = undefined;
-        }
+function startGame() {
+    pF.querySelectorAll('td').forEach(function (e) {
+        console.log(e);
 
+        e.setAttribute('data-id', id++);
+        e.onclick = function () {
+            document.getElementById("playField").setAttribute('style', "background-image:url(" + bg + ");background-size:cover;background-position:center;");
 
-        for (var i = 0; i <= (size * size - 1); i++) {
-            pF.rows[Math.floor((i) / size)].cells[(i) % size].setAttribute('style', 'background:orange;');
-        }
-
-        checkway(startId, count);
-        console.log("checkStartCell ", checkStartCell, checkStartCell == (size * size))
-
-        for (cell in board["table"]) {
-            if (board["table"][cell].start == true) {
-                checkStartCell++;
-                console.log(checkStartCell);
+            HUEdeg = HUEdeg + 5;
+            playSoundClick();
+            var setAttr = "tdblock bl " + current_figure_id;
+            var tempThis = this;
+            this.setAttribute('class', setAttr);
+            var id = this.dataset.id;
+            if ((id == endId) || (id == startId)) {
+                return
             }
-        }
-
-        if ((board["table"][endId].start == true) && (checkStartCell == (size * size)) && (correctWays==true)) {
-            pF.rows[Math.floor((endId) / size)].cells[(endId) % size].innerHTML = "WIN!";
-            playSoundStart();
+            board.writeChanges(id, false);
+            document.getElementById("move").innerHTML = ++move;
+            document.getElementById(current_figure_id).setAttribute('style', 'filter:sepia(100%);order:0; z-index:0;transform:translateY(0px);background-image: url(img/card_general.png);');
+            document.getElementById("body").setAttribute('style', "background-image:url(" + bodyBG + ");background-size:cover;background-position:" + BGposX + "% 50%;");
+            if (BGposX < 100) {
+                BGposX++
+            } else {
+                BGposX = 0
+            };
+            document.getElementById("win").innerHTML = "win";
+            correctWays = true;
             for (var i = 0; i <= (size * size - 1); i++) {
-                pF.rows[Math.floor((i) / size)].cells[(i) % size].setAttribute('style', 'background:' + FULLtransparent + ';--varBG2: #00ffff00;outline: dotted 1px #80808000;');
+                pF.rows[Math.floor((i) / size)].cells[(i) % size].setAttribute('style', 'background:' + FULLtransparent + ';');
             }
+            current_figure_rand();
+            board.checkWin();
+            var count = 0;
+            if (board["table"][endId] == "start") {
+                document.getElementById("win").innerHTML = "!!!";
+                return;
+            }
+            for (cell in board["table"]) {
+                board["table"][cell].start = undefined;
+            }
+
+
+            for (var i = 0; i <= (size * size - 1); i++) {
+                pF.rows[Math.floor((i) / size)].cells[(i) % size].setAttribute('style', 'background:orange;');
+            }
+
+            checkway(startId, count);
+            console.log("checkStartCell ", checkStartCell, checkStartCell == (size * size))
+
+            for (cell in board["table"]) {
+                if (board["table"][cell].start == true) {
+                    checkStartCell++;
+                    console.log(checkStartCell);
+                }
+            }
+
+            if ((board["table"][endId].start == true) && (checkStartCell == (size * size)) && (correctWays == true)) {
+                pF.rows[Math.floor((endId) / size)].cells[(endId) % size].innerHTML = "WIN!";
+                playSoundStart();
+                for (var i = 0; i <= (size * size - 1); i++) {
+                    pF.rows[Math.floor((i) / size)].cells[(i) % size].setAttribute('style', 'background:' + FULLtransparent + ';--varBG2: #00ffff00;outline: dotted 1px #80808000;');
+                }
+            }
+
         }
 
-    }
+        function checkway(Id, recurs) {
 
-    function checkway(Id, recurs) {
+            recurs++;
 
-        recurs++;
+            if ((board["table"][Id].top == 1) && (board["table"][Id - size] != undefined) && (board["table"][Id - size].bottom == 1) && (board["table"][Id - size].start == undefined)) {
+                board["table"][Id - size].start = true;
+                pF.rows[Math.floor((Id - size) / size)].cells[(Id - size) % size].setAttribute('style', 'background:' + BGtransparent + ';');
+                if (recurs < size * size) {
+                    checkway(Id - size, recurs);
+                }
+            }
+            if ((board["table"][Id].bottom == 1) && (board["table"][Id + size] != undefined) && (board["table"][Id + size].top == 1) && (board["table"][Id + size].start == undefined)) {
+                board["table"][Id + size].start = true;
+                pF.rows[Math.floor((Id + size) / size)].cells[(Id + size) % size].setAttribute('style', 'background:' + BGtransparent + ';');
+                if (recurs < (size * size)) {
+                    checkway(Id + size, recurs);
+                }
+            }
+            if ((board["table"][Id].right == 1) && (board["table"][Id + 1] != undefined) && (board["table"][Id + 1].left == 1) && (board["table"][Id + 1].start == undefined)) {
+                board["table"][Id + 1].start = true;
+                pF.rows[Math.floor((Id + 1) / size)].cells[(Id + 1) % size].setAttribute('style', 'background:' + BGtransparent + ';');
+                if (recurs < (size * size)) {
+                    checkway(Id + 1, recurs);
+                }
+            }
+            if ((board["table"][Id].left == 1) && (board["table"][Id - 1] != undefined) && (board["table"][Id - 1].right == 1) && (board["table"][Id - 1].start == undefined)) {
+                board["table"][Id - 1].start = true;
+                pF.rows[Math.floor((Id - 1) / size)].cells[(Id - 1) % size].setAttribute('style', 'background:' + BGtransparent + ';');
+                if (recurs < (size * size)) {
+                    checkway(Id - 1, recurs);
+                }
+            }
 
-        if ((board["table"][Id].top == 1) && (board["table"][Id - size] != undefined) && (board["table"][Id - size].bottom == 1) && (board["table"][Id - size].start == undefined)) {
-            board["table"][Id - size].start = true;
-            pF.rows[Math.floor((Id - size) / size)].cells[(Id - size) % size].setAttribute('style', 'background:' + BGtransparent + ';');
-            if (recurs < size * size) {
-                checkway(Id - size, recurs);
-            }
         }
-        if ((board["table"][Id].bottom == 1) && (board["table"][Id + size] != undefined) && (board["table"][Id + size].top == 1) && (board["table"][Id + size].start == undefined)) {
-            board["table"][Id + size].start = true;
-            pF.rows[Math.floor((Id + size) / size)].cells[(Id + size) % size].setAttribute('style', 'background:' + BGtransparent + ';');
-            if (recurs < (size * size)) {
-                checkway(Id + size, recurs);
-            }
-        }
-        if ((board["table"][Id].right == 1) && (board["table"][Id + 1] != undefined) && (board["table"][Id + 1].left == 1) && (board["table"][Id + 1].start == undefined)) {
-            board["table"][Id + 1].start = true;
-            pF.rows[Math.floor((Id + 1) / size)].cells[(Id + 1) % size].setAttribute('style', 'background:' + BGtransparent + ';');
-            if (recurs < (size * size)) {
-                checkway(Id + 1, recurs);
-            }
-        }
-        if ((board["table"][Id].left == 1) && (board["table"][Id - 1] != undefined) && (board["table"][Id - 1].right == 1) && (board["table"][Id - 1].start == undefined)) {
-            board["table"][Id - 1].start = true;
-            pF.rows[Math.floor((Id - 1) / size)].cells[(Id - 1) % size].setAttribute('style', 'background:' + BGtransparent + ';');
-            if (recurs < (size * size)) {
-                checkway(Id - 1, recurs);
-            }
-        }
-
-    }
-})};
+    })
+};
